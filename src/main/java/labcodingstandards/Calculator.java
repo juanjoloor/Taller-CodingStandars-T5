056a21c2b77df6e251032893abe57dc932cce7dd
xecarlier <xecarlie@espol.edu.ec>
2021-06-09 20:42:36 -0500

    first commit

---
+++ src/main/java/labcodingstandards/Calculator.java
@@ -0,0 +1,90 @@
+// Copyright (C) 2020
+// All rights reserved
+
+package labcodingstandards;
+
+import java.util.Scanner;
+
+
+/**
+ * @author T5_XC
+ */
+public class Calculator {
+	/**
+	 * Main method.
+	 * @author T5_XC
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Scanner reader = new Scanner(System.in);
+
+        System.out.print("1. +\n2. -\n3. *\n4. /\nEnter an operator: ");
+
+        char operator = reader.nextLine().charAt(0);
+        double first;
+        double second;
+        String input;
+
+        while (true) {
+        	System.out.print("Enter first number: ");
+        	input = reader.nextLine();
+
+            try {
+            	first = Integer.parseInt(input);
+                break;
+            } catch (NumberFormatException e) {
+                System.out.println("Not valid!");
+            }
+        }
+
+        while (true) {
+        	System.out.print("Enter second number: ");
+        	input = reader.nextLine();
+
+            try {
+            	second = Integer.parseInt(input);
+                break;
+            } catch (NumberFormatException e) {
+                System.out.println("Not valid!");
+            }
+        }
+
+        Calculator cal = new Calculator();
+        String result = cal.operation(first, second, operator);
+
+        System.out.printf(result);
+		reader.close();
+	}
+	/**
+	 * Performs an operation based on the operator char.
+	 * 1 => +
+	 * 2 => -
+	 * 3 => *
+	 * 4 => /
+	 * @author T5_XC
+	 * @param first The first number
+	 * @param second The second number
+	 * @param operator Determines the operation
+	 * @return String result
+	 */
+	private String operation(double first, double second, char operator) {
+		double result = 0;
+		switch (operator) {
+            case '1':
+                result = first + second;
+                break;
+            case '2':
+                result = first - second;
+                break;
+            case '3':
+                result = first * second;
+                break;
+            case '4':
+                result = first / second;
+                break;
+            default:
+            	return "Error! operator is not correct";
+        }
+		return "The result is: " + result;
+	}
+}
